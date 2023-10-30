53a1734a2bd90ade6caa4fd8e802ad3726fa38a5
dhksdl04 <111281575+dhksdl04@users.noreply.github.com>
2023-10-30 17:37:22 +0900

    [양정완]index.html(사이드바)

---
+++ src/main/resources/static/assets/js/sidebar.js
@@ -0,0 +1,30 @@
+const tabs = document.querySelectorAll('.tab');
+const tabViews = document.querySelectorAll('.tab-view');
+
+tabs.forEach((tab, index) => {
+    tab.addEventListener('click', () => {
+        // 모든 탭 뷰 숨기기
+        tabViews.forEach((view) => {
+            view.style.display = 'none';
+        });
+
+        // 클릭한 탭에 해당하는 탭 뷰만 보이게 하기
+        tabViews[index].style.display = 'block';
+    });
+});
+
+const challTabs = document.querySelectorAll('.active-tab, .finish-tab');
+const challTabViews = document.querySelectorAll('.active-chall-listView, .finish-chall-listView');
+
+challTabs.forEach((challTab, index) => {
+    challTab.addEventListener('click', () => {
+        // 모든 진행중, 종료 탭 뷰 숨기기
+        challTabViews.forEach((view) => {
+            view.style.display = 'none';
+        });
+
+        // 클릭한 진행중, 종료 탭에 해당하는 탭 뷰만 보이게 하기
+        challTabViews[index].style.display = 'block';
+
+    });
+});
