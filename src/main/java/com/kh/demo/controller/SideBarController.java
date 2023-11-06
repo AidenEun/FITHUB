ac2a9c000539cd30e92de7483a8a41fa5eefca31
dhksdl04 <111281575+dhksdl04@users.noreply.github.com>
2023-11-06 21:37:40 +0900

    [양정완]SideBarController.java

--- src/main/java/com/kh/demo/controller/SideBarController.java
+++ src/main/java/com/kh/demo/controller/SideBarController.java
@@ -1,8 +1,8 @@
 package com.kh.demo.controller;
 
-import com.kh.demo.domain.dto.TrainerDTO;
+
 import com.kh.demo.domain.dto.UserDTO;
-import com.kh.demo.service.UserService;
+import com.kh.demo.service.UserMyPageService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,37 +10,35 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.security.Principal;
+
 @Controller
 @RequestMapping("/sidebar/*")
 public class SideBarController {
 
-    @Autowired
-    private UserService userService;
-
-    //나중에 필요시 수정예정
+//    private final UserMyPageService userMyPageService;
+//
 //    @Autowired
-//    private TrainerService trainerService;
-
-    //사용자 정보를 가져오는 로직
-    @GetMapping("user_myinfo")
-    public String sidebarInfo(Model model, HttpSession session) {
-        String userId = (String) session.getAttribute("loginUser");
-        UserDTO user = userService.getDetail(userId);
-        model.addAttribute("user", user);
-        return "index";
-    }
-
-    //나중에 필요시 수정예정
-
-//    @GetMapping("trainer_info")
-//    public String sidebarTrainerInfo(Model model, HttpSession session) {
-//        String userId = (String) session.getAttribute("loginUser");
-//        TrainerDTO trainer = trainerService.getTrainerInfo(userId);
-//        model.addAttribute("trainer", trainer);
-//        return "index";
+//    public SideBarController(UserMyPageService userMyPageService) {
+//        this.userMyPageService = userMyPageService;
 //    }
+
+//    @GetMapping("userMyInfo")
+//    public String userMyInfo(Model model, Principal principal) {
+//        String loginUser = principal.getName();
+//        UserDTO user = userMyPageService.getUserDetail(loginUser);
+//        model.addAttribute("user", user);
+//        // 이후 필요한 다른 데이터도 가져와서 모델에 추가할 수 있습니다.
+//        // 예: List<DiaryDTO> diaryList = userMyPageService.getDiaryList(loginUser);
+//        // model.addAttribute("diaryList", diaryList);
+//        return "sidebar/userMyInfo";
+//
+//    }
+
+
     @GetMapping("#")
     public void replace(){}
+
 
     @GetMapping("bmi")
     public void BMI(){}
