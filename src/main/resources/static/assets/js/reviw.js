7807b1dc52f748eb56ac904042ff970a3d41c7f4
kieun <rldms05@naver.com>
2023-11-13 17:44:04 +0900

    [정기은] 리뷰작성 중

---
+++ src/main/resources/static/assets/js/reviw.js
@@ -0,0 +1,105 @@
+/*
+function f(){
+	return {};
+}
+const replyService = f();
+*/
+const reviewService = (function(){
+	function insert(review,callback){
+		$.ajax({
+			type:"POST",
+			url:"/review/regist",
+			data:JSON.stringify(review),
+			contentType:"application/json;charset=utf-8",
+			success:function(result,status,xhr){
+				callback(result);
+			}
+		});
+	}
+	
+	function selectAll(data,callback){
+		let boardNum = data.boardNum;
+		let pageNum = data.pageNum;
+		
+		$.getJSON(
+			"/reply/pages/"+boardNum+"/"+pageNum,
+			function(data){
+				//data : {replyCnt:댓글개수, list:[....]}
+				callback(data.reviewCnt, data.list);
+			}
+		)
+	}
+	
+	function drop(reviewNum,callback,error){
+		$.ajax({
+			type:"DELETE",
+			url:"/review/"+reviewNum,
+			success:function(result,status,xhr){
+				callback(result);
+			},
+			error:function(xhr,status,err){
+				error(err);	
+			}
+		})
+	}
+	
+	function update(review,callback,error){
+		$.ajax({
+			type:"PUT",
+			url:"/review/"+review.reviewNum,
+			data:JSON.stringify(review),
+			contentType:"application/json;charset=utf-8",
+			success:function(result){
+				if(callback){
+					callback(result);
+				}
+			},
+			error:function(err){
+				if(error){
+					error(err);
+				}
+			}
+		})
+	}
+	
+	function fmtTime(review){
+		const regdate = reply.regdate;
+		const updatedate = reply.updatedate;
+		
+		const now = new Date();
+		
+		const check = regdate == updatedate;
+		
+		const dateObj = new Date(check ? regdate : updatedate);
+		//date객체.getTime() : date객체가 가지고 있는 시간 정보를 밀리초 단위로 추출
+		let gap = now.getTime() - dateObj.getTime();
+		
+		let str = "";
+		if(gap < 1000*60*60*24){
+			let hh = dateObj.getHours();
+			let mi = dateObj.getMinutes();
+			let ss = dateObj.getSeconds();
+			str = (hh>9?'':'0')+hh+":"+(mi>9?'':'0')+mi+":"+(ss>9?'':'0')+ss;
+		}
+		else{
+			let yy = dateObj.getFullYear();
+			let mm = dateObj.getMonth()+1;
+			let dd = dateObj.getDate();
+			
+			str = (yy>9?'':'0')+yy+"/"+(mm>9?'':'0')+mm+":"+(dd>9?'':'0')+dd;
+		}
+		return (check?'':'(수정됨) ')+str;
+	}
+	
+	
+	return {add:insert, getList:selectAll, remove:drop, modify:update, displayTime:fmtTime}
+})();
+
+
+
+
+
+
+
+
+
