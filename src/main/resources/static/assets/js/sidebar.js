12a085776e8e1f707e396ee2e605bf12c0f52e52
dhksdl04 <111281575+dhksdl04@users.noreply.github.com>
2023-11-20 14:37:04 +0900

    [양정완]사이드바 자바스크립트 오류수정

--- src/main/resources/static/assets/js/sidebar.js
+++ src/main/resources/static/assets/js/sidebar.js
@@ -25,7 +25,6 @@ challTabs.forEach((challTab, index) => {
 
         // 클릭한 진행중, 종료 탭에 해당하는 탭 뷰만 보이게 하기
         challTabViews[index].style.display = 'block';
-
     });
 });
 
@@ -34,9 +33,54 @@ document.addEventListener('DOMContentLoaded', function () {
     const attBtn = document.querySelector('.att-btn');
     const pointDisplay = document.getElementById('pointDisplay');
 
-    let filledGauges = 0; // 현재 출석 게이지가 찬 횟수
-    let totalPoints = parseInt(pointDisplay.innerText); // 초기 포인트 설정
-    let alreadyAttended = false; // 출석 여부 확인
+    console.log('.progress-bar:', progressBar);
+    console.log('.att-btn:', attBtn);
+    console.log('pointDisplay:', pointDisplay);
+
+    if (!progressBar || !attBtn || !pointDisplay) {
+        console.error('하나 이상의 요소를 찾을 수 없습니다.');
+        return;
+    }
+
+    let filledGauges = 0;
+    let totalPoints = pointDisplay ? parseInt(pointDisplay.innerText) || 0 : 0;
+
+    attBtn.addEventListener('click', function () {
+        filledGauges++;
+        progressBar.value = filledGauges;
+
+        if (filledGauges >= 7) {
+            totalPoints += 10;
+            pointDisplay.innerText = totalPoints + 'p';
+
+            setTimeout(function () {
+                progressBar.value = 0;
+                filledGauges = 0;
+            }, 1000);
+
+            alert('축하합니다! 포인트 10점을 획득하셨습니다!');
+        } else {
+            alert('출석이 완료되었습니다.');
+        }
+    });
+
+/*document.addEventListener('DOMContentLoaded', function () {
+    const progressBar = document.querySelector('.progress-bar');
+    const attBtn = document.querySelector('.att-btn');
+    const pointDisplay = document.getElementById('pointDisplay');
+
+    console.log('.progress-bar:', progressBar);
+    console.log('.att-btn:', attBtn);
+    console.log('pointDisplay:', pointDisplay);
+
+    if (!progressBar || !attBtn || !pointDisplay) {
+        console.error('One or more elements not found.');
+        return;
+    }
+
+    let filledGauges = 0;
+    let totalPoints = pointDisplay ? parseInt(pointDisplay.innerText) || 0 : 0;
+    let alreadyAttended = false;
 
     attBtn.addEventListener('click', function () {
         if (!alreadyAttended) {
@@ -44,57 +88,43 @@ document.addEventListener('DOMContentLoaded', function () {
             progressBar.value = filledGauges;
 
             if (filledGauges === 7) {
-                // 7회 출석 완료 시 포인트 지급
-                totalPoints += 10; // 10 포인트로 테스트
-
-                // 포인트 표시 갱신
+                totalPoints += 10;
                 pointDisplay.innerText = totalPoints + 'p';
 
-                // 출석 게이지 초기화
                 setTimeout(function () {
                     progressBar.value = 0;
                     filledGauges = 0;
                 }, 1000);
 
-                // 축하 및 포인트 지급 알림 메시지
                 alert('축하합니다! 포인트 10점을 획득하셨습니다!');
             } else {
-                // 출석이 완료되었습니다. 알림 메시지
                 alert('출석이 완료되었습니다.');
             }
 
-            // 출석 여부 갱신
             alreadyAttended = true;
         } else {
-            // 중복 출석 경고 메시지
             alert('오늘은 이미 출석을 완료하였습니다.');
         }
-    });
-});
+    });*/
 
-document.addEventListener("DOMContentLoaded", function() {
-    // 모든 탭 요소와 컨텐츠 요소를 가져옵니다.
-    const tabs = document.querySelectorAll("#meal-options > div");
-    const contents = document.querySelectorAll("#tan-dan-ji-box > div");
+    document.addEventListener("DOMContentLoaded", function () {
+            const tabs = document.querySelectorAll("#meal-options > div");
+            const contents = document.querySelectorAll("#tan-dan-ji-box > div");
 
-    // 모든 컨텐츠를 숨깁니다.
-    contents.forEach(content => {
-        content.style.display = "none";
-    });
-
-    // 아침 탭에 해당하는 컨텐츠를 표시합니다.
-    contents[0].style.display = "block";
-
-    // 각 탭에 클릭 이벤트 리스너를 추가합니다.
-    tabs.forEach((tab, index) => {
-        tab.addEventListener("click", function() {
-            // 모든 컨텐츠를 숨깁니다.
             contents.forEach(content => {
                 content.style.display = "none";
             });
 
-            // 클릭한 탭에 해당하는 컨텐츠를 표시합니다.
-            contents[index].style.display = "block";
+            contents[0].style.display = "block";
+
+            tabs.forEach((tab, index) => {
+                tab.addEventListener("click", function () {
+                    contents.forEach(content => {
+                        content.style.display = "none";
+                    });
+
+                    contents[index].style.display = "block";
+                });
+            });
         });
     });
-});
