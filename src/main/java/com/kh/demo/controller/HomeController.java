f3370869e722d1316d91f6e29f33e15f0ec551e5
cjw2500 <147566447+cjw2500@users.noreply.github.com>
2023-11-24 13:55:17 +0900

    feat:[최재우] 다이어리 간단히 보기 구현

--- src/main/java/com/kh/demo/controller/HomeController.java
+++ src/main/java/com/kh/demo/controller/HomeController.java
@@ -14,6 +14,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -57,7 +58,8 @@ public class HomeController {
     @Qualifier("UserServiceImpl")
     private UserService serviceUser;
 
-
+    @Autowired
+    private CalorieService calorieService;
 
 
     @RequestMapping("/")
@@ -81,18 +83,182 @@ public class HomeController {
         //유저 게시글 댓글 최다작성 랭킹 1-5위
         List<UserDTO> userReplyTotalTop5List = serviceUser.getUserReplyTotalTop5List();
         model.addAttribute("userReplyTotalTop5List",userReplyTotalTop5List);
-
         //인기 게시글 랭킹 1-5위
         List<BoardDTO> userBoardLikeTop5List = boardservice.getUserBoardLikeTop5List();
         model.addAttribute("userBoardLikeTop5List",userBoardLikeTop5List);
-        System.out.println("userBoardLikeTop5List: "+userBoardLikeTop5List);
 
-        DiaryDTO todayDiary = umpservice.getTodayDiary(loginUser);
-        model.addAttribute("todayDiary",todayDiary);
+        LocalDate todaydate = LocalDate.now();
+        System.out.println("todaydate: "+todaydate);
+        model.addAttribute("todaydate", todaydate);
+
+        UserDTO user = serviceUser.getDetail(loginUser);
+//        DiaryDTO diary = umpservice.getDiaryDetail(todayDiary.getRegdate(), loginUser);
+
+        if (user != null) {
+            DiaryDTO diary = umpservice.getTodayDiary(loginUser);
+            if (diary != null) {
+                //오늘의 비교 계산
+                double resultweight = Math.round((user.getWeightGoal() - diary.getTodayWeight()) * 100.0) / 100.0;
+                //오늘 칼로리 계산
+                double totalResult = Math.round(diary.getFoodCalories() - diary.getExerCalories()) * 100.0 / 100.0;
+
+                //각 음식, 운동 num 번호가 있는 배열
+                String bfNumlist = diary.getTodayBreakfast();
+                String lunchNumlist = diary.getTodayLunch();
+                String dinnerNumlist = diary.getTodayDinner();
+                String snackNumlist = diary.getTodaySnack();
+                String exerNumlist = diary.getTodayExer();
+
+                String[] todayAlllist = {bfNumlist, lunchNumlist, dinnerNumlist, snackNumlist, exerNumlist};
+                String breakfast = "";
+                String lunch = "";
+                String dinner = "";
+                String snack = "";
+                String exer = "";
+                double bfCal = 0d;
+                double lunchCal = 0d;
+                double dinnerCal = 0d;
+                double snackCal = 0d;
+
+
+                for (int i = 0; i < todayAlllist.length; i++) {
+    //            System.out.println(todayAlllist[i]);
+                    if (todayAlllist[i].isEmpty()) {
+    //                System.out.println("빈문자열"+ i);
+                    } else {
+                        if (i == 0) {
+                            String[] bfArr = todayAlllist[i].split(",");
+                            ArrayList<String> bfnameList = new ArrayList<String>();
+                            for (String data : bfArr) {
+                                List<FoodDTO> bfDTOList = calorieService.findfoodName(data);
+                                for (FoodDTO dtoData : bfDTOList) {
+                                    bfnameList.add(dtoData.getFoodName());
+                                    bfCal += Double.parseDouble(dtoData.getFoodCalories());
+
+                                }
+                            }
+                            bfCal = Math.round(bfCal * 100.0) / 100.0;
+                            for (int j = 0; j < bfnameList.size(); j++) {
+                                String breakfast1 = "";
+                                if (j == bfnameList.size() - 1) {
+                                    breakfast1 += bfnameList.get(j);
+                                } else {
+                                    breakfast1 += (bfnameList.get(j) + ",");
+                                }
+                                breakfast += breakfast1;
+
+                            }
+
+                        } else if (i == 1) {
+                            String[] lunchArr = todayAlllist[i].split(",");
+                            ArrayList<String> lunchnameList = new ArrayList<String>();
+                            for (String data : lunchArr) {
+                                List<FoodDTO> lunchDTOList = calorieService.findfoodName(data);
+                                for (FoodDTO dtoData : lunchDTOList) {
+                                    lunchnameList.add(dtoData.getFoodName());
+                                    lunchCal += Double.parseDouble(dtoData.getFoodCalories());
+                                }
+                            }
+                            lunchCal = Math.round(lunchCal * 100.0) / 100.0;
+                            for (int j = 0; j < lunchnameList.size(); j++) {
+                                String lunch1 = "";
+                                if (j == lunchnameList.size() - 1) {
+                                    lunch1 += lunchnameList.get(j);
+                                } else {
+                                    lunch1 += (lunchnameList.get(j) + ",");
+                                }
+                                lunch += lunch1;
+
+                            }
+                        } else if (i == 2) {
+                            String[] dinnerArr = todayAlllist[i].split(",");
+                            ArrayList<String> dinnernameList = new ArrayList<String>();
+                            for (String data : dinnerArr) {
+                                List<FoodDTO> dinnerDTOList = calorieService.findfoodName(data);
+                                for (FoodDTO dtoData : dinnerDTOList) {
+                                    dinnernameList.add(dtoData.getFoodName());
+                                    dinnerCal += Double.parseDouble(dtoData.getFoodCalories());
+                                }
+                            }
+                            dinnerCal = Math.round(dinnerCal * 100.0) / 100.0;
+                            for (int j = 0; j < dinnernameList.size(); j++) {
+                                String dinner1 = "";
+                                if (j == dinnernameList.size() - 1) {
+                                    dinner1 += dinnernameList.get(j);
+                                } else {
+                                    dinner1 += (dinnernameList.get(j) + ",");
+                                }
+                                dinner += dinner1;
+
+                            }
+                        } else if (i == 3) {
+                            String[] snackArr = todayAlllist[i].split(",");
+                            ArrayList<String> snacknameList = new ArrayList<String>();
+                            for (String data : snackArr) {
+                                List<FoodDTO> snackDTOList = calorieService.findfoodName(data);
+                                for (FoodDTO dtoData : snackDTOList) {
+                                    snacknameList.add(dtoData.getFoodName());
+                                    snackCal += Double.parseDouble(dtoData.getFoodCalories());
+                                }
+                            }
+                            snackCal = Math.round(snackCal * 100.0) / 100.0;
+                            for (int j = 0; j < snacknameList.size(); j++) {
+                                String snack1 = "";
+                                if (j == snacknameList.size() - 1) {
+                                    snack1 += snacknameList.get(j);
+                                } else {
+                                    snack1 += (snacknameList.get(j) + ",");
+                                }
+                                snack += snack1;
+
+                            }
+                        } else if (i == 4) {
+                            String[] exerArr = todayAlllist[i].split(",");
+                            ArrayList<String> exernameList = new ArrayList<String>();
+                            for (String data : exerArr) {
+                                List<ExerciseDTO> exerDTOList = calorieService.findExecName(data);
+                                for (ExerciseDTO dtoData : exerDTOList) {
+                                    exernameList.add(dtoData.getExecName());
+                                }
+                            }
+                            for (int j = 0; j < exernameList.size(); j++) {
+                                String exer1 = "";
+                                if (j == exernameList.size() - 1) {
+                                    exer1 += exernameList.get(j);
+                                } else {
+                                    exer1 += (exernameList.get(j) + ",");
+                                }
+                                exer += exer1;
+
+                            }
+                        } else {
+                            System.out.println("해당사항없음 오류");
+                        }
+
+                    }
+                }
+                double[] foodCalArr = {bfCal, lunchCal, dinnerCal, snackCal};
+                String[] listNameArr = {breakfast, lunch, dinner, snack, exer};
+                String[] sccChall;
+                //진행중인 챌린지 내역
+                List<MyChallengeDTO> myChallINGDTOList = challService.findMychall(diary.getUserId(), diary.getRegdate());
+                //성공한 챌린지 내역
+    //        System.out.println("성공한 챌린지 번호 : "+diary.getMyChallNum());
+
+
+            model.addAttribute("resultweight", resultweight);
+            model.addAttribute("listNameArr", listNameArr);
+            model.addAttribute("foodCalArr", foodCalArr);
+            model.addAttribute("totalResult", totalResult);
+            model.addAttribute("myChallINGDTOList", myChallINGDTOList);
+            model.addAttribute("sccChallNum", diary.getMyChallNum());
+            }
+            model.addAttribute("diary", diary);
+        }
+        model.addAttribute("user", user);
 
         List<ChallNoticeBoardDTO> challNoticeBoardList = challService.getChallNoticeBoardList(20);
         model.addAttribute("challNoticeBoardList",challNoticeBoardList);
-        System.out.println("challNoticeBoardList: "+challNoticeBoardList);
 
 //        List<BoardDTO> indexInfoBoardList = boardservice.getindexInfoBoardList();
 //        model.addAttribute("indexInfoBoardList",indexInfoBoardList);
